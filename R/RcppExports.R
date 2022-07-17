5ecefcb5f9f77d0d7541d62ad6b1c28562aef4e6
seokhoonj <seokhoonj@gmail.com>
2022-07-17 16:08:28 +0900

    registration is added

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,6 +2,6 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 rcpp_backtest <- function(gr, wt, init, cont, rebal) {
-    .Call(`_firer_rcpp_backtest`, gr, wt, init, cont, rebal)
+    .Call('_firer_rcpp_backtest', PACKAGE = 'firer', gr, wt, init, cont, rebal)
 }
 
